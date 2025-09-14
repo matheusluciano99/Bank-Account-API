d9d9c2adf083d659907ba9071cde6e2bd6fd229f
matheusluciano99 <matheusluciano99@gmail.com>
2025-09-14 12:54:28 -0300

    chore: spring init

---
+++ Conta_Bancaria_API/src/main/java/arq/obj/ContaBancariaApiApplication.java
@@ -0,0 +1,13 @@
+package arq.obj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaApiApplication.class, args);
+	}
+
+}
---
+++ Conta_Bancaria_API/src/test/java/arq/obj/ContaBancariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package arq.obj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e0c1352f5a6762403e35377f9d05d160e77fd8fb
matheusluciano99 <matheusluciano99@gmail.com>
2025-10-09 20:58:15 -0300

    feat: adicionando repository para as classes

---
+++ Conta_Bancaria_API/src/main/java/arq/obj/Classes/Usuario/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package arq.obj.Classes.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    public Usuario findByEmail(String email);
+}
